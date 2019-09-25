4427835fc984ce54940e67ee4cb0d7a19b043f68
keatonwilson <keatonwilson@me.com>
2019-09-25 14:50:12 -0700

    Master function through adding blockCV objects.

--- sdm_function.R
+++ sdm_function.R
@@ -2,8 +2,7 @@
 # Keaton Wilson
 # keatonwilson@me.com
 # 2019-09-20
-
-
+# 
 # packages ----------------------------------------------------------------
 
 library(dismo)
@@ -40,7 +39,7 @@ bv_t2 = readRDS("./data/bioclim_t2.rds")
 #'  to the area of the occurences for each subset split by year_split.
 #'
 #' @examples
-prep_data = function(data, year_split = 2000) {
+prep_data = function(data, year_split = 2000, env_raster_t1, env_raster_t2) {
   
   # selecting the pieces we want and separating by time
   small_data = data %>%
@@ -57,8 +56,8 @@ prep_data = function(data, year_split = 2000) {
   geographic_extent <- extent(x = c(min_lon-1, max_lon+1, min_lat-1, max_lat+1))
   
   # Crop bioclim data to geographic extent of species
-  bv_t1_cropped <- crop(x = bv_t1, y = geographic_extent)
-  bv_t2_cropped <- crop(x = bv_t2, y = geographic_extent)
+  bv_t1_cropped <- crop(x = env_raster_t1, y = geographic_extent)
+  bv_t2_cropped <- crop(x = env_raster_t2, y = geographic_extent)
   
   # Split by time period into two data frames
   df_t1 = small_data %>% 
@@ -269,37 +268,94 @@ full_model = function(models_obj, best_model_index, full_data = NULL, env_raster
   FC_best = as.character(auc_mod$features[1])
   rm_best = auc_mod$rm
   
+  
   maxent.args = ENMeval::make.args(RMvalues = rm_best, fc = FC_best)
+  
+  # re calculating environmental raster
+  
 
-  full_mod = maxent(env_data, as.matrix(full_data[,1:2]), args = maxent.args[[1]])
+  full_mod = maxent(env_raster, as.matrix(full_data[,1:2]), args = maxent.args[[1]])
   return(full_mod)
 }
 
+
+# Master Function - build_sdm() -------------------------------------------
+
+build_sdm = function(multi_species_df, year_split, env_raster_t1, env_raster_t2){
+  # split multi-species dataframe into a list
+  butt_list = split(multi_species_df, f = multi_species_df$true_name)
+  
+  # Iterating the prep_data function over the list of species dataframes
+  prepped_data_list = lapply(butt_list, 
+                             prep_data, 
+                             env_raster_t1 = env_raster_t1, 
+                             env_raster_t2 = env_raster_t2, 
+                             year_split = year_split)
+  
+  # Generating blockCV objects for each time period for each species and attaching to master list
+
+  for(i in 1:length(prepped_data_list)){
+    # initializing the list
+    block_list = list()
+    
+    # Calculating stuff
+    block_t1 = run_block_cv(prepped_data = prepped_data_list[[i]][[1]][[1]], 
+                            bv_raster = prepped_data_list[[i]][[2]][[1]])
+    block_t2 = run_block_cv(prepped_data = prepped_data_list[[i]][[1]][[2]], 
+                            bv_raster = prepped_data_list[[i]][[2]][[2]])
+    
+    block_list = list(t1_block = block_t1, t2_block = block_t2)
+    prepped_data_list[[i]]$blocks = block_list
+  }
+  
+  return(prepped_data_list)
+  }
+
 # Testing Sandbox ---------------------------------------------------------
+# Loading saved temp objects
+best_model = readRDS("./tmp/best_mod.rds")
+block_test = readRDS("./tmp/block_test.rds")
+ev = readRDS("./tmp/evaluation_obj.rds")
+eval = readRDS("./tmp/models.rds")
+
 
 test_data = read_csv("./data/candidate_occurences.csv") %>%
   filter(true_name == "Leptotes marina")
 
 test_prepped = prep_data(test_data)
-block_test = run_block_cv(test_prepped[[1]][[1]], test_prepped[[2]][[1]])
+#block_test = run_block_cv(test_prepped[[1]][[1]], test_prepped[[2]][[1]])
 
 prep_2_test = prep_data_2(data = test_prepped[[1]][[1]], env_raster = test_prepped[[2]][[1]])
 
 split_test = train_test_split(prep_2_test, block_test)
 
 # Takes forever to run
-eval = model_func(data = split_test[[1]], env_raster = test_prepped[[2]][[1]])
+#eval = model_func(data = split_test[[1]], env_raster = test_prepped[[2]][[1]])
 
 # plots
 eval_plots(eval)
 
 # best mod
-best_model = best_mod(eval)
+#best_model = best_mod(eval)
 
 # ev obj
 
-ev = evaluate_models(test_data = prep_data_2(test_prepped[[1]][[2]], 
-                                             env_raster = test_prepped[[2]][[2]]),
-                     model = best_model, 
-                     env_raster = test_prepped[[2]][[2]])
+# ev = evaluate_models(test_data = prep_data_2(test_prepped[[1]][[2]], 
+#                                              env_raster = test_prepped[[2]][[2]]),
+#                      model = best_model, 
+#                      env_raster = test_prepped[[2]][[2]])
 
+
+full_mod = full_model(models_obj = eval, 
+                      best_model_index = best_model[[2]], 
+                      full_data = test_data[,2:3],
+                      env_raster = test_prepped[[2]][[1]])
+
+#Testing master function
+full_data = read_csv("./data/candidate_occurences.csv")
+unique(full_data$true_name)
+
+small_test_data = full_data %>%
+  filter(true_name == "Leptotes marina" | true_name == "Vanessa atalanta")
+
+t = build_sdm(multi_species_df = small_test_data, year_split = 2000, env_raster_t1 = bv_t1, env_raster_t2 = bv_t2)
