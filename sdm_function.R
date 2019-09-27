e0e998d8768e091b8d513036f0fd1c358d9f1a30
keatonwilson <keatonwilson@me.com>
2019-09-27 09:59:16 -0700

    Continuing to build out master function. Added some error handling (try) and some quality control upfront.

--- sdm_function.R
+++ sdm_function.R
@@ -197,7 +197,7 @@ return(list(training_data = training_data,
 
 # Modeling ----------------------------------------------------------------
 
-model_func = function(data = NULL, env_raster) {
+model_func = function(data = NULL, env_raster, num_cores) {
   data_occ = data %>%  #Generating occurence lat long
     filter(Species == 1) %>%
     dplyr::select(longitude, latitude) %>%
@@ -214,6 +214,8 @@ model_func = function(data = NULL, env_raster) {
                      env = env_raster,
                      method = 'randomkfold', 
                      kfolds = 5, 
+                     parallel = TRUE,
+                     numCores = num_cores
                      algorithm = 'maxent.jar')
   return(eval)
 }
@@ -281,13 +283,41 @@ full_model = function(models_obj, best_model_index, full_data = NULL, env_raster
 
 # Master Function - build_sdm() -------------------------------------------
 
-build_sdm = function(multi_species_df, year_split, env_raster_t1, env_raster_t2){
+build_sdm = function(multi_species_df, year_split, env_raster_t1, env_raster_t2, num_cores){
+  # Setting seed for reproducibility
+  
+  set.seed(42)
+  
+  # QC to make sure we have enough records for each species
+  data_summary = multi_species_df %>%
+    filter(!is.na(year)) %>%
+    mutate(time_frame = ifelse(year > 1999, "T1", "T2")) %>%
+    group_by(true_name, time_frame) %>%
+    summarize(n = n()) 
+  
+  if(any(data_summary$n < 10)) {
+    print("Removing species with less than 10 records for a given period")
+    
+    good_list = multi_species_df %>%
+      filter(!is.na(year)) %>%
+      mutate(time_frame = ifelse(year > 1999, "T1", "T2")) %>%
+      group_by(true_name, time_frame) %>%
+      summarize(n = n()) %>%
+      filter(n >= 10) %>%
+      select(true_name)
+    
+    multi_species_df = multi_species_df %>% 
+      filter(true_name %in% good_list)
+    
+    
+  }
+  
   # split multi-species dataframe into a list
   butt_list = split(multi_species_df, f = multi_species_df$true_name)
   
   # Iterating the prep_data function over the list of species dataframes
   prepped_data_list = lapply(butt_list, 
-                             prep_data, 
+                             try(prep_data), 
                              env_raster_t1 = env_raster_t1, 
                              env_raster_t2 = env_raster_t2, 
                              year_split = year_split)
@@ -299,17 +329,85 @@ build_sdm = function(multi_species_df, year_split, env_raster_t1, env_raster_t2)
     block_list = list()
     
     # Calculating stuff
-    block_t1 = run_block_cv(prepped_data = prepped_data_list[[i]][[1]][[1]], 
-                            bv_raster = prepped_data_list[[i]][[2]][[1]])
-    block_t2 = run_block_cv(prepped_data = prepped_data_list[[i]][[1]][[2]], 
-                            bv_raster = prepped_data_list[[i]][[2]][[2]])
+    block_t1 = try(run_block_cv(prepped_data = prepped_data_list[[i]][[1]][[1]], 
+                            bv_raster = prepped_data_list[[i]][[2]][[1]]))
+    block_t2 = try(run_block_cv(prepped_data = prepped_data_list[[i]][[1]][[2]], 
+                            bv_raster = prepped_data_list[[i]][[2]][[2]]))
     
     block_list = list(t1_block = block_t1, t2_block = block_t2)
     prepped_data_list[[i]]$blocks = block_list
   }
   
-  return(prepped_data_list)
+  # Running second prepping data function
+  
+  for(j in 1:length(prepped_data_list)){
+    prepped_df_t1 = try(prep_data_2(data = prepped_data_list[[j]]$data[[1]], 
+                                env_raster = prepped_data_list[[j]]$env_data[[1]]))
+    prepped_df_t2 = try(prep_data_2(data = prepped_data_list[[j]]$data[[2]], 
+                                env_raster = prepped_data_list[[j]]$env_data[[2]]))
+    data_df = list(prepped_df_t1, prepped_df_t2)
+    prepped_data_list[[j]]$prepped_dfs = data_df
   }
+  
+  # Training and test split
+  for(j in 1:length(prepped_data_list)){
+    training_list_t1 = try(train_test_split(prepped_data_list[[j]]$prepped_dfs[[1]],
+                                        blocked_obj = prepped_data_list[[j]]$blocks[[1]]))
+    training_list_t2 = try(train_test_split(prepped_data_list[[j]]$prepped_dfs[[2]],
+                                        blocked_obj = prepped_data_list[[j]]$blocks[[2]]))
+    prepped_data_list[[j]]$train_test_split[[1]] = training_list_t1
+    prepped_data_list[[j]]$train_test_split[[2]] = training_list_t2
+    
+  }
+  
+  # Modeling
+  for(k in 1:length(prepped_data_list)){
+    models_t1 = try(model_func(data = prepped_data_list[[k]]$train_test_split[[1]]$training_data, 
+                           env_raster = prepped_data_list[[k]]$env_data[[1]]))
+    models_t2 = try(model_func(data = prepped_data_list[[k]]$train_test_split[[2]]$training_data, 
+                           env_raster = prepped_data_list[[k]]$env_data[[2]]))
+    prepped_data_list[[k]]$models = list(models_t1, models_t2)
+  }
+  
+  # Choosing best model
+  for(l in 1:length(prepped_data_list)){
+    best_mod_t1 = try(best_mod(model_obj = prepped_data_list[[k]]$models[[1]]))
+    best_mod_t2 = try(best_mod(model_obj = prepped_data_list[[k]]$models[[2]]))
+    prepped_data_list[[k]]$best_mod = list(best_mod_t1, best_mod_t2)
+  }
+  
+  # evaluating best model on blockCV test data
+  for(m in 1:length(prepped_data_list)){
+    ev_t1 = try(evaluate_models(test_data = prepped_data_list[[m]]$train_test_split[[1]]$test_data,
+                            model = prepped_data_list[[m]]$best_mod[[1]],
+                            env_raster = prepped_data_list[[m]]$env_data[[1]]))
+    ev_t2 = try(evaluate_models(test_data = prepped_data_list[[m]]$train_test_split[[2]]$test_data,
+                            model = prepped_data_list[[m]]$best_mod[[2]],
+                            env_raster = prepped_data_list[[m]]$env_data[[2]]))
+    
+    prepped_data_list[[m]]$evaluations = list(ev_t1, ev_t2)
+  }
+  
+  # full mods on all data
+  for(n in 1:length(prepped_data_list)){
+    full_mod_t1 = try(full_model(models_obj = prepped_data_list[[n]]$models[[1]],
+                             best_model_index = prepped_data_list[[n]]$best_mod[[1]][[2]],
+                             full_data = butt_list[[n]] %>%
+                               filter(year < year_split), 
+                             env_raster = prepped_data_list[[n]]$env_data[[1]]
+                             ))
+    
+    full_mod_t2 = try(full_model(models_obj = prepped_data_list[[n]]$models[[2]],
+                             best_model_index = prepped_data_list[[n]]$best_mod[[2]][[2]],
+                             full_data = butt_list[[n]] %>%
+                               filter(year >= year_split), 
+                             env_raster = prepped_data_list[[n]]$env_data[[2]]))
+    
+    prepped_data_list[[n]]$full_mods = list(full_mod_t1, full_mod_t2)
+
+  }
+  return(prepped_data_list)
+}
 
 # Testing Sandbox ---------------------------------------------------------
 # Loading saved temp objects
@@ -355,7 +453,23 @@ full_mod = full_model(models_obj = eval,
 full_data = read_csv("./data/candidate_occurences.csv")
 unique(full_data$true_name)
 
-small_test_data = full_data %>%
-  filter(true_name == "Leptotes marina" | true_name == "Vanessa atalanta")
+sum(is.na(full_data$year))
+sum(is.na(full_data$eventDate))
 
-t = build_sdm(multi_species_df = small_test_data, year_split = 2000, env_raster_t1 = bv_t1, env_raster_t2 = bv_t2)
+full_data %>%
+  filter(!is.na(year)) %>%
+  mutate(time_frame = ifelse(year > 1999, "T1", "T2")) %>%
+  group_by(true_name, time_frame) %>%
+  summarize(n = n())
+
+small_test_data = full_data %>%
+  filter(true_name == "Danaus plexippus")
+
+
+t = build_sdm(multi_species_df = small_test_data, year_split = 2000, env_raster_t1 = bv_t1, env_raster_t2 = bv_t2, num_cores = 2)
+
+
+# Need to build in some QC checking on the data to feed in... the function breaks 
+# when a given species doesn't have enough occurence data to split into 5 chunks for 
+# cross validation. Also, get rid of NAs in date/year
+# 
